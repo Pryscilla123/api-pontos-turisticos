63b967f5bdf8f6e1f35584efef1965daf7ba1991
Pryscilla Medeiros <pryscilla.medeiros@enacom.com.br>
2023-07-21 16:54:07 -0300

    Criando endpoint de avaliações

--- avaliacoes/models.py
+++ avaliacoes/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 
 
 class Avaliacao(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comentarios = models.TextField(null=True, blank=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    comentario = models.TextField(null=True, blank=True)
     nota = models.DecimalField(max_digits=3, decimal_places=2)
     data = models.DateTimeField(auto_now_add=True)
 
