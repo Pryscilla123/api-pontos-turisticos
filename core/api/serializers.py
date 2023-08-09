41c88c4d81b8f23c6254ce4d9c2a4c92380220b7
Pryscilla Medeiros <pryscilla.medeiros@enacom.com.br>
2023-08-09 16:33:34 -0300

    Implementando nested relationships no serializer dos pontos turísticos

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,8 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from core.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer
+from enderecos.api.serializers import EnderecoSerializer
 
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracaoSerializer(many=True)
+    enderecos = EnderecoSerializer()
+    descricao_completa = SerializerMethodField()
+
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto')
+        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto', 'atracoes', 'comentarios', 'avaliacoes', 'endereco')
+
+    def get_descricao_completa(self, obj):
+        return '%s - %s' % (obj.nome, obj.descricao)
